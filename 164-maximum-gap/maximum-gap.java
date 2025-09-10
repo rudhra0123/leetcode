7349b2d6e89c059508a6970cd3fd6e46ff8c5792
sivaramaraju <22a91a0584@gmail.com>
2025-09-10 14:46:10 +0530

    Time: 41 ms (34.84%) | Memory: 60.8 MB (64.33%) - LeetSync

--- 164-maximum-gap/maximum-gap.java
+++ 164-maximum-gap/maximum-gap.java
@@ -1,49 +1,11 @@
 class Solution {
-    public int maximumGap(int[] nums)
-    {
-        int lnt=nums.length;
-        mergesort(nums,0,lnt-1);
-        System.out.println(Arrays.toString(nums));
+    public int maximumGap(int[] nums) {
+        if(nums.length<2) return 0;
+        Arrays.sort(nums);
         int max=0;
-        for(int i=0;i<lnt-1;i++)
-        {
+        for(int i=0;i<nums.length-1;i++){
             max=Math.max(max,nums[i+1]-nums[i]);
         }
         return max;
-
-    }
-    public int[] mergesort(int[] arr,int left,int right)
-    {
-        if(left<right)
-        {
-            int mid=(left+right)/2;
-            mergesort(arr,left,mid);
-            mergesort(arr,mid+1,right);
-            merge(arr,left,mid,right);
-        }
-        return arr;
-    }
-    public void merge(int[] arr,int left,int mid,int right)
-    {
-        int n1=mid-left+1;
-        int n2=right-mid;
-        int[] l1=new int[n1];
-        int[] r1=new int[n2];
-        for(int i=0;i<n1;i++)
-        l1[i]=arr[left+i];
-        for(int i=0;i<n2;i++)
-        r1[i]=arr[mid+i+1];
-        int i=0,j=0,k=left;
-        while(i<n1 && j<n2)
-        {
-            if(l1[i]<=r1[j])
-            arr[k++]=l1[i++];
-            else
-            arr[k++]=r1[j++];
-        }
-        while(i<n1)
-        arr[k++]=l1[i++];
-        while(j<n2)
-        arr[k++]=r1[j++];
     }
 }
