ecadcafccfc1e0ad1789595894906d47f0887523
sivaramaraju <22a91a0584@gmail.com>
2026-02-07 20:44:37 +0530

    Time: 0 ms (100.00%) | Memory: 43.1 MB (20.98%) - LeetSync

---
+++ 2243-check-if-all-as-appears-before-all-bs/check-if-all-as-appears-before-all-bs.java
@@ -0,0 +1,14 @@
+class Solution {
+    public boolean checkString(String s)
+    {
+       boolean a=true;
+       for(int i=0;i<s.length()-1;i++)
+       {
+        if(s.charAt(i)=='b' && s.charAt(i+1)=='a')
+        {
+           a=false; 
+        }
+       }
+       return a; 
+    }
+}
